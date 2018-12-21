ff2d395345ac8fd870b847a87125b820a5fffe88
Nikhil Vedi <nikhil.vedi@skybettingandgaming.com>
2018-12-21 10:35:50 +0000

    player

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express'),
   logger = require('morgan'),
   app = express(),
-  PdfReader = require('pdfreader'),
-  fs = require("fs"),
   template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+
+import YouTubePlayer from 'youtube-player';
+
+let player;
+player = YouTubePlayer('video-player');
+player.loadVideoById('dQw4w9WgXcQ');
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/static'));
@@ -28,6 +32,10 @@ app.get('/gitflow', function(req, res) {
    var file = __dirname + '/Upload-folder/gitflow.pdf';
     res.download(file);
 });
+
+app.get('/rickroll', function(req, res) {
+    player.playVideo();
+})
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
